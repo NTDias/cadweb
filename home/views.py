fef6aeafe2c15071353160deb6da86d76b5504f1
Netto Dias <pereirasecinfo@gmail.com>
2025-12-23 15:15:01 -0300

    Aula dia 19

--- home/views.py
+++ home/views.py
@@ -1,6 +1,8 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 
 from .models import *
+from .forms import *
+
 
 
 def index(request):
@@ -11,3 +13,18 @@ def categoria(request):
         'lista': Categoria.objects.all().order_by('-id'),
     }
     return render(request, 'categoria/lista.html',contexto)
+
+def form_categoria(request):
+    if request.method == 'POST':
+       form = CategoriaForm(request.POST)                        # instancia o modelo com os dados do form
+       if form.is_valid():                                       # faz a validação do formulário
+            form.save()                                          # salva a instancia do modelo no banco de dados
+            return redirect('categoria')                         # redireciona para a listagem
+    else:                                                        # método é get, novo registro
+        form = CategoriaForm()
+
+
+    contexto = {
+        'form':form,
+    }
+    return render(request, 'categoria/formulario.html', contexto)    
